07cb7d9f9e64f5ee34734070284cd6c47e9c37de
TR\pbanda <bpraveen078@gmail.com>
2019-04-29 02:16:31 +0530

    added login

--- packages/edist-web/src/App.tsx
+++ packages/edist-web/src/App.tsx
@@ -13,19 +13,107 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Drawer from "@material-ui/core/Drawer";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import { IThemeModel } from "../src/theme/theme";
+import classNames from "classnames";
 
 import { createBrowserHistory } from "history";
+import {
+  List,
+  IconButton,
+  Divider,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  createStyles
+} from "@material-ui/core";
 
 export const history = createBrowserHistory();
-const styles = {
-  root: {
-    flexGrow: 1
-  }
-};
 
-class App extends React.Component {
+const drawerWidth = 240;
+const styles = (theme: IThemeModel) =>
+  createStyles({
+    root: {
+      display: "flex"
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      transition: theme.transitions.create(["width", "margin"], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen
+      })
+    },
+    appBarShift: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(["width", "margin"], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen
+      })
+    },
+    menuButton: {
+      marginLeft: 12,
+      marginRight: 36
+    },
+    hide: {
+      display: "none"
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+      whiteSpace: "nowrap"
+    },
+    drawerOpen: {
+      width: drawerWidth,
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen
+      })
+    },
+    drawerClose: {
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen
+      }),
+      overflowX: "hidden",
+      width: theme.spacing.unit * 7 + 1,
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing.unit * 9 + 1
+      }
+    },
+    toolbar: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "flex-end",
+      padding: "0 8px",
+      ...theme.mixins.toolbar
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing.unit * 3
+    }
+  });
+
+export interface IAppProps {
+  open: boolean;
+  close: boolean;
+  classes: any;
+  theme: any;
+}
+
+export interface IAppState {
+  open: boolean;
+  close: boolean;
+}
+
+class App extends React.Component<IAppProps, IAppState> {
   constructor(props: any) {
     super(props);
+    this.state = { open: true, close: false };
   }
   public handleDrawerOpen = () => {
     this.setState({ open: true });
@@ -35,6 +123,7 @@ class App extends React.Component {
     this.setState({ open: false });
   };
   public render() {
+    const { classes, theme } = this.props;
     const csvData = [
       ["firstname", "lastname", "email"],
       ["Ahmed", "Tomi", "ah@smthing.co.com"],
@@ -46,13 +135,76 @@ class App extends React.Component {
         <PropsContext.Provider value={makeAppContext}>
           <Router history={history}>
             <div>
-              <AppBar position="static" color="default">
-                <Toolbar>
-                  <Typography variant="h6" color="inherit">
-                    Photos
+              <AppBar
+                position="fixed"
+                className={classNames(classes.appBar, {
+                  [classes.appBarShift]: this.state.open
+                })}
+              >
+                <Toolbar disableGutters={!this.state.open}>
+                  <IconButton
+                    color="inherit"
+                    aria-label="Open drawer"
+                    onClick={this.handleDrawerOpen}
+                    className={classNames(classes.menuButton, {
+                      [classes.hide]: this.state.open
+                    })}
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                  <Typography variant="h6" color="inherit" noWrap={true}>
+                    Mini variant drawer
                   </Typography>
                 </Toolbar>
               </AppBar>
+              <Drawer
+                variant="permanent"
+                className={classNames(classes.drawer, {
+                  [classes.drawerOpen]: this.state.open,
+                  [classes.drawerClose]: !this.state.open
+                })}
+                classes={{
+                  paper: classNames({
+                    [classes.drawerOpen]: this.state.open,
+                    [classes.drawerClose]: !this.state.open
+                  })
+                }}
+                open={this.state.open}
+              >
+                <div className={classes.toolbar}>
+                  <IconButton onClick={this.handleDrawerClose}>
+                    {theme.direction === "rtl" ? (
+                      <ChevronRightIcon />
+                    ) : (
+                      <ChevronLeftIcon />
+                    )}
+                  </IconButton>
+                </div>
+                <Divider />
+                <List>
+                  {["Inbox", "Starred", "Send email", "Drafts"].map(
+                    (text, index) => (
+                      <ListItem button={true} key={text}>
+                        <ListItemIcon>
+                          {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                      </ListItem>
+                    )
+                  )}
+                </List>
+                <Divider />
+                <List>
+                  {["All mail", "Trash", "Spam"].map((text, index) => (
+                    <ListItem button={true} key={text}>
+                      <ListItemIcon>
+                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                      </ListItemIcon>
+                      <ListItemText primary={text} />
+                    </ListItem>
+                  ))}
+                </List>
+              </Drawer>
               {/* <ButtonOne disable /> */}
               {/* <TestComponent id={1} /> */}
               <div>
@@ -72,4 +224,5 @@ class App extends React.Component {
   }
 }
 
-export default App;
+// export default App;
+export default withStyles(styles, { withTheme: true })(App);
--- packages/edist-web/src/pages/common/login.page.tsx
+++ packages/edist-web/src/pages/common/login.page.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import Button from "@material-ui/core/Button";
+import TextInput from "src/elements/text-input";
 
 export interface ILoginPageProps {
   error: string;
@@ -7,12 +9,68 @@ export interface ILoginPageProps {
 }
 export interface ILoginPageState {
   isLoading: boolean;
+  login: {
+    userName: string;
+    password: string;
+  };
 }
 class Login extends React.Component<ILoginPageProps, ILoginPageState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      login: {
+        userName: "",
+        password: ""
+      }
+    };
+  }
+  public handleSubmit = (e: any) => {
+    console.log(e);
+  };
+  public handleChange = (e: any) => {
+    const { login } = this.state;
+    const { name, value } = e.target;
+
+    this.setState({
+      login: {
+        ...login,
+        [name]: value
+      }
+    });
+  };
   public render() {
+    const { userName, password } = this.state.login;
     return (
-      <div>
-        <div>dfdfr</div>
+      <div style={{ marginTop: "155px" }}>
+        Login
+        <form name="loginForm" onSubmit={this.handleSubmit}>
+          <TextInput
+            onChange={this.handleChange}
+            value={userName}
+            required={true}
+            minLength={4}
+            label="User Name"
+            name="userName"
+            placeholder="User Name"
+            maxLength={20}
+          />
+          <br />
+          <TextInput
+            onChange={this.handleChange}
+            value={password}
+            required={true}
+            minLength={4}
+            label="Password"
+            name="password"
+            placeholder="Password"
+            maxLength={10}
+          />
+          <br />
+          <Button variant="contained" color="primary">
+            Login
+          </Button>
+        </form>
       </div>
     );
   }
